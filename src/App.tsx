53b80b1440c4170943d7c9c7ec6f7275aa496e7c
Noshin Nawar <noshinnawar06@gmail.com>
2026-09-12 23:02:30 +0600

    challenge

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,11 @@ import './App.css'
 import Navbar from "./Navbar";
 import Hero from "./Hero";
 import TechnologyGrid from "./TechnologyGrid";
-import type {Technology} from "./types";
+import type { Technology } from "./types";
 import rawData from "./technologies.json";
 import Footer from "./Footer";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 function App() {
   const [technologies, setTechnologies] = useState<Technology[]>([]);
@@ -21,18 +23,37 @@ function App() {
   }, []);
 
   const addToStack = (tech: Technology) => {
-    if (stack.find((t) => t.id === tech.id)) return; 
+    const alreadyAdded = stack.find((t) => t.id === tech.id);
+
+    if (alreadyAdded) {
+      toast.warning(`${tech.name} is already in your stack`);
+      return;
+    }
+
     setStack([...stack, tech]);
+    toast.success(`${tech.name} added to your stack`);
   };
 
   const removeFromStack = (id: string) => {
+    const removedTech = stack.find((t) => t.id === id);
     setStack(stack.filter((t) => t.id !== id));
+
+    if (removedTech) {
+      toast.info(`${removedTech.name} remove from your stack`);
+    }
   };
 
-  const removeAll = () => setStack([]);
+
+  const removeAll = () => {
+    setStack([]);
+    toast.info("Your stack has been cleared.");
+  };
+
+
 
   return (
     <div>
+      <ToastContainer position="top-right" autoClose={2000} />
       <Navbar />
       <Hero />
       {loading ? (
